091899359bd73e5887933922a1d43a3f146e9a81
Anonymous_PC <anonymous_pc_serbia@protonmail.com>
2022-08-18 11:16:45 -0400

    Got project environment setup with proper debug.
    Got project environment setup with proper debug.

---
+++ main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert("working!");
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLGtCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcHJvamVjdF90b2RvX2xpc3QvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiYWxlcnQoXCJ3b3JraW5nIVwiKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
