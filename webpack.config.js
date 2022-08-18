6bf2590dd717e2f27299e202777c8aa3e9fdd089
Anonymous_PC <anonymous_pc_serbia@protonmail.com>
2022-08-18 11:33:42 -0400

    Set webpack up to use .css files, images and fonts.
    Set webpack up to use .css files, images and fonts.

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,25 @@ module.exports = {
     devtool: 'inline-source-map',
     devServer: {
         static: './dist',
-      },
+    },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
     },
 };
