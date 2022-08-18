9a88d622bfaab1012e5c250b326f3291e2807113
Anonymous_PC <anonymous_pc_serbia@protonmail.com>
2022-08-18 17:24:47 -0400

    Completed title area.
    Completed title area.

---
+++ src/createTitle.js
@@ -0,0 +1,28 @@
+const getTitle = () => {
+    return document.querySelector(".title");
+}
+
+const addIcon = (icon_name) => {
+    const span = document.createElement("span");
+    span.classList.add("material-icons");
+    span.innerHTML = icon_name;
+    return span;
+}
+
+const addText = (text) => {
+    const textNode = document.createTextNode(text);
+    return textNode;
+}
+
+const createNavObject = (icon_name, text) => {
+    const container = document.createElement("div");
+    container.classList.add("container");
+    container.append(addIcon(icon_name), addText(text));
+    return container;
+}
+
+function appendToTitle() {
+    getTitle().append(createNavObject("task_alt", "Todo List"));
+}
+
+appendToTitle();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './style.css';
 import './createGrid.js';
 import './createNav.js';
+import './createTitle';
