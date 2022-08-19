563985d88631a2066ebfe709b79c56458402d02e
Anonymous_PC <anonymous_pc_serbia@protonmail.com>
2022-08-19 11:59:15 -0400

    Implemented task creation.
    Implemented task creation.

--- src/createBody.js
+++ src/createBody.js
@@ -1,4 +1,8 @@
 let sectionId = "Inbox";
+let taskName = "";
+const getContent = () => {
+    return document.getElementById("content");
+}
 
 const getBody = () => {
     return document.querySelector(".body");
@@ -29,28 +33,83 @@ const addIcon = (icon_name) => {
     return span;
 }
 
+const createInput = (input_type,
+    id,
+    listener_bool,
+    listener_type,
+    listener_function
+) => {
+    const input = document.createElement("input");
+    input.type = input_type;
+    input.setAttribute("id", id);
+
+    if (listener_bool)
+        input.addEventListener(listener_type, listener_function);
+
+    return input;
+}
+
+const createLabel = () => {
+    const label = document.createElement("label");
+    label.for = taskName;
+    label.innerHTML = taskName;
+    return label;
+}
+
+const createInputContainer = (label, task, date) => {
+    const container = document.createElement("div");
+    container.classList.add("inputContainer");
+    container.append(label, task, date);
+    return container;
+}
+
+function closeModal() {
+    document.querySelector(".modal").remove();
+    addTask();
+}
+
+function setTaskName() {
+    taskName = this.value;
+}
+
+function createTaskModal() {
+    const modal = document.createElement("div");
+    modal.classList.add("modal");
+    const modalContent = document.createElement("div");
+    modalContent.classList.add("modal-content");
+    modalContent.append(
+        "Please name your task...",
+        createInput("text", "createTask", true, "input", setTaskName),
+        createButton("check", "Create Task", closeModal)
+    );
+    modal.appendChild(modalContent);
+    getContent().append(modal);
+}
+
 function addTask() {
-    alert("Add Task!");
+    // alert("Add Task!");
+    const taskContainer = document.createElement("div");
+    const form = document.createElement("form");
+    form.append(createInputContainer(createLabel(taskName), createInput("text", taskName, false, null, null), createInput("date", taskName, false, null, null)));
+    taskContainer.append(form);
+    getBody().append(taskContainer);
 }
 
-const addEvtListener = (button) => {
-    button.addEventListener("mousedown", addTask)
-}
-
-const createButton = (icon, text) => {
+const createButton = (icon, text, funct) => {
     const button = document.createElement("button");
     button.append(addIcon(icon));
     button.append(text);
-    addEvtListener(button);
+    button.addEventListener("mousedown", funct);
     button.classList.add("button")
     return button;
 }
 
-
-
 const createBodyObject = () => {
     if (sectionId === "Inbox") {
-        return createButton("add", "Add Task");
+        const inboxContainer = document.createElement("div");
+        inboxContainer.classList.add("inboxContainer");
+        inboxContainer.append(createButton("add", "Add Task", createTaskModal));
+        return inboxContainer;
     }
     else if (sectionId === "Today") {
 
@@ -61,7 +120,7 @@ const createBodyObject = () => {
 }
 
 function appendToBody() {
-    getBody().append(createBodyObject());
+    getBody().append(createTitle(), createBodyObject());
 }
 
 appendToBody();
